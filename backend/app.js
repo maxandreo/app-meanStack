e3cb746565a836a349646a4c2266bdfe872fd5d1
Max Andreo <andreo.max@gmail.com>
2019-05-03 12:59:58 +0200

    Ajout du backend NodeJs et modif des composants Angular concernant les posts

---
+++ backend/app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
+// Fetch un Post reçue en POST
+app.post("/api/posts", (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added successfully'
+  });
+});
+
+// Fetch une liste de Posts en GET
+app.get("/api/posts",(req, res, next) => {
+  const posts = [
+    {
+      id: "fad45616",
+      title: "First server-side post",
+      content: "This is coming from the server"
+    },
+    {
+      id: "dfg5468",
+      title: "Second server-side post",
+      content: "This is coming from the server!"
+    }
+  ];
+  res.status(200).json({
+    message: 'Posts fetched successfully!',
+    posts: posts
+  });
+});
+
+module.exports = app;
